b7b768835290c31b7ca9ca480c64cc7d0b794ab5
Ben Kuhl <ben.kuhl@realpage.com>
2016-08-08 15:09:25 -0400

    Adding ssh

--- infrastructure/start.sh
+++ infrastructure/start.sh
@@ -7,18 +7,17 @@
 # Add private key to server
 if [ "${PRIVATE_KEY}" != "**None**" ]; then
     echo "=> Found private key"
-    mkdir -p /home/www-data/.ssh
-    chmod 700 /home/www-data/.ssh
-    echo "=> Adding private key to /home/www-data/.ssh/id_rsa"
+    mkdir -p /root/.ssh
+    chmod 700 /root/.ssh
+    echo "=> Adding private key to /root/.ssh/id_rsa"
 
     # private key is encoded since multiline environment variable support is janky
-    echo -e "$PRIVATE_KEY" | base64 -d > /home/www-data/.ssh/id_rsa
+    echo -e "$PRIVATE_KEY" | base64 -d > /root/.ssh/id_rsa
 
     # avoid "The authenticity of host 'xxxxx' can't be established." when cloning
-    echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> /home/www-data/.ssh/config
+    echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
 
-    chmod 600 /home/www-data/.ssh/id_rsa
-    chown -R www-data:www-data /home/www-data
+    chmod 600 /root/.ssh/id_rsa
 else
     echo "ERROR: No private keys found in \$PRIVATE_KEY"
    exit 1
