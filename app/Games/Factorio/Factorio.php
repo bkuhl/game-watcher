8afd3bdbeb066419323c1a67cf864f339d6ba8d2
Ben Kuhl <ben.kuhl@realpage.com>
2017-05-10 13:50:15 -0400

    Re-enabled sha

--- app/Games/Factorio/Factorio.php
+++ app/Games/Factorio/Factorio.php
@@ -66,13 +66,13 @@ class Factorio implements PublishesVersions
         $dockerfilePath = $repository->path().'/'.$version->major().'.'.$version->minor().'/Dockerfile';
         if ($this->filesystem->exists($dockerfilePath)) {
             // sha1 disabled since this doesn't change between major versions
-            //$sha1 = $this->sha1($version);
+            $sha1 = $this->sha1($version);
 
             // update Dockerfile with latest details
             $dockerfile = $this->filesystem->get($dockerfilePath);
             $replacements = [
                 "/VERSION=(.*?) \\\\/"  => 'VERSION='.$version->version().' \\',
-                //"/SHA1=(.*)/"          => 'SHA1='.$sha1,
+                "/SHA1=(.*)/"          => 'SHA1='.$sha1,
             ];
             $dockerfile = preg_replace(array_keys($replacements), array_values($replacements), $dockerfile);
             $this->filesystem->put($dockerfilePath, $dockerfile);
